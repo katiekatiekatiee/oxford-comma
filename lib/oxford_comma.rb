71ec2effb7932dce26c3d07909cd3fb468531c7e
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-02 23:08:18 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    case array.size 
+    when 0
+        ""
+    when 1 
+        array[0].to_s
+    when 2
+        array.join(" and ")
+    else 
+        new_array = array
+        new_array[-1] = "and #{new_array[-1]}"
+        new_array.join(", ")
+    end
 end
